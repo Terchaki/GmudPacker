af515e39aa7050a2d3b6c3b4f217cf0d0f19944f
Lucas Henrique Sousa Mendes <lucas.smendes@invillia.com>
2026-06-18 09:42:43 -0300

    feat: adicionar funcionalidade de visualização de imagens no tutorial

--- gmudPacker/src/app/components/tutorial/tutorial.component.ts
+++ gmudPacker/src/app/components/tutorial/tutorial.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
@@ -9,4 +9,19 @@ import { RouterLink } from '@angular/router';
   templateUrl: './tutorial.component.html',
   styleUrl: './tutorial.component.scss'
 })
-export class TutorialComponent {}
+export class TutorialComponent {
+  selectedImage: { src: string; alt: string } | null = null;
+
+  openImage(src: string, alt: string): void {
+    this.selectedImage = { src, alt };
+  }
+
+  closeImage(): void {
+    this.selectedImage = null;
+  }
+
+  @HostListener('document:keydown.escape')
+  onEscapeKey(): void {
+    this.closeImage();
+  }
+}
